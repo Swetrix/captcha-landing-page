3cd9039cee6e354b7215104e8a6feda0089cfccc
Blue Mouse <durexp@protonmail.ch>
2023-05-20 22:39:01 +0100

    seo improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 tailwind.config = {
   mode: 'jit',
-  important: true,
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
@@ -18,30 +17,12 @@ tailwind.config = {
           750: '#141d2e',
         }
       },
-      width: {
-        'card-toggle': 'calc(100% - 3rem)',
-      },
-      animation: {
-        'ping-slow': 'ping-op 3s cubic-bezier(0, 0, 0.2, 1) infinite',
-        'appear': 'appear 0.5s cubic-bezier(0, 0, 0.2, 1) 1',
-      },
-      keyframes: {
-        'ping-op': {
-          '0%': {
-            opacity: 0.75,
-          },
-          '75%, 100%': {
-            transform: 'scale(2)',
-            opacity: 0,
-          },
-        },
-      },
     },
     minHeight: {
-      '72': '18rem',
-      'page': 'calc(100vh - 74px)', // header height -> 74px
-      'min-footer': 'calc(100vh - 162px)', // 74px + 88px footer height
-      'min-footer-ad': 'calc(100vh - 265px)', // 74px + 88px footer height
+     '72': '18rem',
+     'page': 'calc(100vh - 74px)', // header height -> 74px
+     'min-footer': 'calc(100vh - 160px)', // 74px + 88px footer height
+     'min-footer-ad': 'calc(100vh - 265px)', // 74px + 88px footer height
     },
   },
   variants: {
